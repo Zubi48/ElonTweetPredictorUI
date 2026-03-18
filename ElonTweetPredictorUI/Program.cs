5e979a01c520b776cc0eb6e8985f07dc9b002727
Carlos Zubiate <carlosklr48@gmail.com>
2026-03-17 22:42:09 -0400

    fixing loading

--- ElonTweetPredictorUI/Program.cs
+++ ElonTweetPredictorUI/Program.cs
@@ -1,11 +1,19 @@
 using ElonTweetPredictorUI.Components;
 using ElonTweetPredictorUI.Services;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
 
 builder.Services.AddSingleton<IStatusService, StatusService>();
 builder.Services.AddSingleton<ILogService, LogService>();
@@ -20,8 +28,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseForwardedHeaders();
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
